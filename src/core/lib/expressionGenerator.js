20a990dc116b107060c1e5ad275d1c6493d7a7c3
Ankit Borude <ankitborude250@gmail.com>
2024-09-28 18:11:29 +0530

    Adding expression generator

---
+++ src/core/lib/expressionGenerator.js
@@ -0,0 +1,26 @@
+import { Random } from "random-js";
+const random = new Random();
+
+const generateQueryString = (config)=> {
+  const {
+    maxTerms,
+    minNumber,
+    maxNumber,
+    operators
+  } = config;
+
+    const termCount = random.integer(2, maxTerms);
+    let expression = '';
+
+    for (let i = 0; i < termCount; i++) {
+      // Add a number
+      expression += random.integer(minNumber, maxNumber);
+
+      // Add an operator if this isn't the last term
+      if (i < termCount - 1) {
+        expression += random.pick(operators);
+      }
+    }
+    return expression;
+}
+export default generateQueryString;
