5ff34beae7bd9e0b63a9176ff931fa3d0b001c13
Ankit Borude <ankitborude250@gmail.com>
2024-09-28 18:25:48 +0530

    Added Exam Router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { studentRouter } from "./routes/student.router.js";
 import { adminRouter } from "./routes/admin.router.js";
+import { examRouter } from "./routes/exam.router.js";
 const app=express();
 
 app.use(express.json());
@@ -9,4 +10,5 @@ app.use(express.static("public"));
 
 app.use("/api/v1/student",studentRouter);
 app.use("/api/v1/admin",adminRouter);
+app.use("/api/v1/exam",examRouter);
 export {app};
