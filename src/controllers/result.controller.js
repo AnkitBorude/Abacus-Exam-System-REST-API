434a8035a77e093e610e0208e670c9b280f4387f
Ankit Borude <ankitborude250@gmail.com>
2024-09-29 14:53:43 +0530

    create result controller

--- src/controllers/result.controller.js
+++ src/controllers/result.controller.js
@@ -5,7 +5,23 @@ import { validatefields } from "../utils/validatereqfields.util.js";
 import { Result } from "../models/result.model.js";
 
 const createResult=asyncHandler(async(req,res)=>{
+ const { score, time_taken, total_correct, date_completed,exam } = req.body;
+ let validParams=validatefields({ score, time_taken, total_correct, date_completed,exam });
+    if(validParams.parameterisNull)
+    {
+        throw new Apierror(401,validParams.parameterName+" is null or undefined");
+    }
 
+    const result = new Result({
+        score,
+        time_taken,
+        total_correct,
+        date_completed,//will store the date in UTC thus will make need to append +5:30 each time
+        exam,
+        student:req.user
+    });
+    const finalresult=await result.save();
+    return res.status(200).json(new Apiresponse("Result created Successfully",200));
 });
 
 export {createResult};
