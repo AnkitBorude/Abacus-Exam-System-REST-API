4ce267aa734721a424fa93d9860ab3b9c6876d4d
Ankit Borude <ankitborude250@gmail.com>
2024-10-19 12:25:09 +0530

    Implementing the helmet for secure header policy

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ import { studentRouter } from "./routes/student.router.js";
 import { adminRouter } from "./routes/admin.router.js";
 import { examRouter } from "./routes/exam.router.js";
 import { resultRouter } from "./routes/result.router.js";
-
+import helmet from "helmet";
 const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static("public"));
-
+app.use(helmet());
 app.use("/api/v1/student",studentRouter);
 app.use("/api/v1/admin",adminRouter);
 app.use("/api/v1/exam",examRouter);
