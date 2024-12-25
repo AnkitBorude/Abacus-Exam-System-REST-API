eebff202db73204fe74fb3f8babbd5388d5006ac
Ankit Borude <ankitborude250@gmail.com>
2024-12-25 17:51:19 +0530

    temporary token validation endpoint

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import { adminRouter } from './routes/admin.router.js';
 import { examRouter } from './routes/exam.router.js';
 import { resultRouter } from './routes/result.router.js';
 import helmet from 'helmet';
+import Apierror from './utils/apierror.util.js';
+import jwt from 'jsonwebtoken';
+import process from 'node:process'
 const app = express();
 
 app.use(express.json());
@@ -17,5 +20,23 @@ app.use('/api/v1/result', resultRouter);
 
 app.get('/api/v1/echo', (req, res) => {
     res.json({ echoed: true });
+});
+
+app.post('/api/v1/verifytoken',(req,res)=>{
+
+    const {token}=req.body;
+    try{
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        res.json(decoded);
+    }catch(error)
+    {
+        res.status(error.statusCode || 500).json({
+            error: error.message,
+            statusCode: error.statusCode,
+            timestamp: error.time,
+            success: false,
+        });
+    }
 });
 export { app };
