8019de9abe9120f4c2f2c0dfe5a1e407e46af669
Ankit Borude <ankitborude250@gmail.com>
2025-01-05 17:36:39 +0530

    admin router update

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -4,6 +4,8 @@ import {
     loginAdmin,
     getCurrentAdmin,
     regenerateAccessToken,
+    deleteAdmin,
+    updateAdmin,
 } from '../controllers/admin.controller.js';
 import { adminValidation } from '../middlewares/adminValidation.middleware.js';
 import authMiddleware from '../middlewares/jwtauth.middleware.js';
@@ -11,7 +13,8 @@ const adminRouter = Router();
 
 adminRouter.route('/register').post(adminValidation,registerAdmin);
 adminRouter.route('/login').post(loginAdmin);
-
+adminRouter.route('/').delete(authMiddleware,deleteAdmin);
+adminRouter.route('/').patch(authMiddleware,adminValidation,updateAdmin);
 adminRouter.route('/me').get(authMiddleware, getCurrentAdmin);
 adminRouter.route('/token').post(regenerateAccessToken);
 export { adminRouter };
