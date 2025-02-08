ff6a65a3a009243433bcf59dca0954f36ab6f207
Ankit Borude <ankitborude250@gmail.com>
2025-02-08 10:02:07 +0530

    optimizing pdf generation

--- src/controllers/pdf.controller.js
+++ src/controllers/pdf.controller.js
@@ -1,11 +1,7 @@
-/***********
- * ref:https://chatgpt.com/c/671c792f-07c8-800d-97d4-1dac4c9a59a0
- *
- */
-import createPdf from '../pdftemplets/generator.pdf.js';
-import { Pdftemplet } from '../pdftemplets/pdf.class.js';
+import createPdf from '../utils/pdf/generator.pdf.js';
+// eslint-disable-next-line no-unused-vars
+import { Pdftemplet } from '../utils/pdf/pdf.class.js';
 import Apierror from '../utils/apierror.util.js';
-import Apiresponse from '../utils/apiresponse.util.js';
 /***
  * @async
  * @param {Request} req
@@ -17,16 +13,16 @@ const getPdf = async (req, res, pdfTemplet) => {
         const pdfBuffer = createPdf(pdfTemplet);
         res.setHeader(
             'Content-Disposition',
-            `attachment; filename=student-report.pdf`
+            `attachment; filename=report.pdf`
         );
         res.setHeader('Content-Type', 'application/pdf');
+        
+        // eslint-disable-next-line no-undef
         res.status(200).send(Buffer.from(pdfBuffer));
     } catch (error) {
         console.error('Error generating PDF:', error);
         throw new Apierror(489, 'error while generating pdf' + error.message);
     }
-
-    // res.status(200).json(new Apiresponse("Reached successfully to pdf controller"));
 };
 
 export { getPdf };
